9eff5ece6442203215527d3878bfb3ff8c0cee36
Sivan Mehta <smehta@godaddy.com>
2019-02-14 15:18:52 -0800

    [wip] basic test to read wrhsrc

--- src/wrhsrc.js
+++ src/wrhsrc.js
@@ -1,8 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
 /**
  * Return the .wrhsrc at a given path, or an empty object if no file exists
- * @param  {String}   path     path to repo
+ * @param  {String}   repo     path to repo
  * @param  {Function} callback completion handler
  */
-module.exports = function (path, callback) {
-  callback(null, {});
+module.exports = function (repo, callback) {
+  const file = path.join(repo, '.wrhsrc');
+  fs.readFile(file, 'utf8', (err, data) => {
+    debugger;
+    // there was an issue with reading the file
+    if(err && err.code !== 'ENOENT') {
+      return callback(err);
+    }
+
+    // the file does not exist
+    if(err && err.code === 'ENOENT') {
+      return callback(null, {})
+    }
+
+
+    // the file does exist
+    callback(null, JSON.parse(data));
+  });
 };
--- test/individual.test.js
+++ test/individual.test.js
@@ -1,25 +1,32 @@
+const path = require('path');
+const assume = require('assume');
+
 const wrhsrc = require('../src/wrhsrc');
 const pkg = require('../src/package');
 const toml = require('../src/package');
 
-function test({ fetcher, repo, expectation }, done) {
-  it(`should return ${JSON.stringify(expectation)} from ${repo}`, function () {
+function test({ fetcher, repo, expectation }) {
+  return function (done) {
     fetcher(repo, (err, config) => {
       assume(err).is.falsey();
-      assume(config).equals(expectation);
+      assume(config).deep.equals(expectation);
       done();
     });
-  });
+  }
 }
 
 describe('Reading config from each source', function () {
   describe('.wrhsrc', function () {
-    it('should return the correct config');
-  });
-  describe('package.json', function () {
-    it('should return the correct config');
-  });
-  describe('toml', function () {
-    it('should return the correct config');
+    it('should return the correct config', test({
+      fetcher: wrhsrc,
+      repo: path.join(__dirname, 'fixtures', '.wrhsrc'),
+      expectation: { power: 'unlimited' }
+    }));
+
+    it('should not return anything from a repo that does not have one', test({
+      fetcher: wrhsrc,
+      repo: path.join(__dirname, 'fixtures', 'wrhs.toml'),
+      expectation: { }
+    }));
   });
 });
