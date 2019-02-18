1d9fc80fe3c334f12576f871ac9c2a66e374319e
Sivan Mehta <smehta@godaddy.com>
2019-02-18 11:55:35 -0800

    [nit] linting

--- test/mutltiple.test.js
+++ test/mutltiple.test.js
@@ -5,12 +5,13 @@ const extract = require('../src');
 describe('extract', function () {
   it('returns appropriately merged contents of a repo with multiple config sources', function () {
     extract(path.join(__dirname, 'fixtures', 'multiple'), (err, config) => {
+      assume(err).is.falsey();
       assume(config).deep.equals({
         hello: 'there',
         power: 'unlimited',
         build: 'webpack',
-        locales: [ 'Sokovia', 'Wakanda', 'Latveria' ]
-      })
+        locales: ['Sokovia', 'Wakanda', 'Latveria']
+      });
     });
   });
 });
