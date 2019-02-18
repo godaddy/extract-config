38bbe0a00aaf3921da0d99b6c84b81a9d9125af0
Sivan Mehta <smehta@godaddy.com>
2019-02-18 13:17:57 -0800

    [wip] more clearly enumerate mergable config

--- test/mutltiple.test.js
+++ test/mutltiple.test.js
@@ -1,16 +1,39 @@
 const path = require('path');
 const assume = require('assume');
-const extract = require('../src');
+const proxyquire = require('proxyquire');
+const extract = proxyquire('../src', {
+  './toml': function (repo, callback) {
+    callback(null, {
+      heroes: ['Clark', 'Diana', 'Bruce'],
+      repo: repo + '.toml'
+    });
+  },
+  './package': function (repo, callback) {
+    callback(null, {
+      heroes: ['Barry', 'John', 'Hal'],
+      villians: ['E. Nigma', 'Vandal', 'Sinestro'],
+      repo: repo + '.json'
+    });
+  },
+  './wrhsrc': function (repo, callback) {
+    callback(null, {
+      villians: ['Adrian', 'Victor', 'webpack'],
+      worlds: ['Sokovia', 'Wakanda', 'Latveria'],
+      repo
+    });
+  }
+});
 
 describe('extract', function () {
   it('returns appropriately merged contents of a repo with multiple config sources', function () {
-    extract(path.join(__dirname, 'fixtures', 'multiple'), (err, config) => {
+    const repo = path.join(__dirname, 'fixtures', 'multiple');
+    extract(repo, (err, config) => {
       assume(err).is.falsey();
       assume(config).deep.equals({
-        hello: 'there',
-        power: 'unlimited',
-        build: 'webpack',
-        locales: ['Sokovia', 'Wakanda', 'Latveria']
+        heroes: ['Barry', 'John', 'Hal'],
+        villians: ['Adrian', 'Victor', 'webpack'],
+        worlds: ['Sokovia', 'Wakanda', 'Latveria'],
+        repo
       });
     });
   });
